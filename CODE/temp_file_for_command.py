eaed3b4fa249c611c71610b1354cd421a9dcc791
Chandru-crypted <chander.sekaran12@gmail.com>
2021-03-28 22:09:42 +0530

    Added updating_register_table.py

---
+++ CODE/temp_file_for_command.py
@@ -0,0 +1,6 @@
+# i have used the file for creating the create table command which has 128 columns
+f = open(r"C:\Users\ELCOT\Documents\Chandru\Git_Bash\Attendance_system_using_facial_recognition\CODE\db\table_creation2.txt", "a")
+for i in range(128):
+	f.write("Face_arr_{:} REAL,".format(i))
+	f.write("\n")
+f.close()
