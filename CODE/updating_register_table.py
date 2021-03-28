40142a661e00bf583557ba9fe18577c55be4ddbe
Chandru-crypted <chander.sekaran12@gmail.com>
2021-03-28 22:10:53 +0530

    ADded updating_register.py

--- CODE/updating_register_table.py
+++ CODE/updating_register_table.py
@@ -1,37 +1,83 @@
-import sqlite3
+from sqlite3 import connect
 import face_recognition
-import cv2
+#import cv2
 from pypika import Query, Table
+import time
 
-def creating_insert(table_name, Reg_ID, Face_array):
+
+def execute_insert_or_update(db_loc, command):
+	conn = connect(db_loc)
+	curser = conn.cursor()
+	curser.execute(command)
+	conn.commit()
+	conn.close()
+
+
+def Insert_Reg_ID(db_loc, table_name, Reg_ID):
 	table = Table(table_name)
-	q = Query.into(table).columns('Reg_ID', 'Face_array').insert(Reg_ID, Face_array)
+	q = Query.into(table).columns('Reg_ID').insert(Reg_ID)
 	sql_command = q.get_sql()
-	return (sql_command)
-
-def execute_insert(command):
-	conn = sqlite3.connect(db_loc)
-	curser = conn.execute(command)
-	conn.close() 
-
-def get_back_array_from_string(Face_array):
-	Face_array_list =  list(map(float, Face_array.split(' ')[: -1]))
-
-	print(Face_array_list)
+	execute_insert_or_update(db_loc, sql_command)
 
 
-db_loc = r'C:\Users\ELCOT\Documents\Chandru\Git_Bash\Attendance_system_using_facial_recognition\CODE\db\test.db'
-table_name = "register"
+def Getting_ID_after_Reg_ID(db_loc, table_name, Reg_ID):
+	table = Table(table_name)
+	q = Query.from_(table).select(table.ID).where(table.Reg_ID == Reg_ID)
+	sql_command = q.get_sql()
+	conn = connect(db_loc)
+	curser = conn.cursor()
+	curser.execute(sql_command)
+	ID = tuple(curser)[0]
+	conn.close()
+	return (ID)
+	
 
-known_image = face_recognition.load_image_file(r"C:\Users\ELCOT\Documents\Chandru\Git_Bash\Attendance_system_using_facial_recognition\CODE\Classroom test\After face detection and croping\Cls1.png")
-known_encoding = face_recognition.face_encodings(known_image)[0]
+def Updating_Face_arr(db_loc, table_name, ID,Face_arr_name, Face_arr_value):
+	table = Table(table_name)
+	q = Query.update(table).set(Face_arr_name, Face_arr_value).where(table.ID == ID)
+	sql_command = q.get_sql()
+	execute_insert_or_update(db_loc, sql_command)
 
-Face_array = ""
-for i in range(len(known_encoding)):
-	Face_array += "{:.8f}".format(float(known_encoding[i]))
-	Face_array += " " 
 
-command = creating_insert(table_name, "RA1711004040030", Face_array)
-print(command)
-get_back_array_from_string(Face_array)
-# execute_insert(command)
+def updating_register_table_with_face_array(db_loc,table_name,Reg_ID, face_arr):
+	
+	# inserting the registration number in the table
+	Insert_Reg_ID(db_loc, table_name, Reg_ID)
+
+	# getting the id of the registraion number entered 
+	ID = Getting_ID_after_Reg_ID(db_loc, table_name, Reg_ID)
+
+	for i in range(0, 128):
+		load_start_time = time.time()
+		Face_arr_name = "Face_arr_%s" % (i)
+		load_end_time = time.time()
+		print("making the face arr string ---{:}----".format(load_start_time - load_end_time))
+		load_start_time = time.time()
+		Updating_Face_arr(db_loc, table_name, ID, Face_arr_name, face_arr[i])
+		load_end_time = time.time()
+		print("updating face arr ---{:}----".format(load_start_time - load_end_time))
+
+
+def updating_register_table_with_face_image(db_loc, table_name, Reg_ID, face_img_loc):
+	start_time = time.time()
+	load_start_time = time.time()
+	image = face_recognition.load_image_file(face_img_loc)
+	load_end_time = time.time()
+	print("loading the image ---{:}----".format(load_start_time - load_end_time))
+	load_start_time = time.time()
+	encoding = face_recognition.face_encodings(image)[0]
+	load_end_time = time.time()
+	print("face_recognition process ---{:}----".format(load_start_time - load_end_time))
+	load_start_time = time.time()
+	updating_register_table_with_face_array(db_loc, table_name, Reg_ID, encoding)
+	load_end_time = time.time()
+	print("updation in database ---{:}----".format(load_start_time - load_end_time))
+	end_time = time.time()
+	print("total ---{:}----".format(end_time - start_time))
+
+
+if __name__ == "__main__":
+	db_loc = r'C:\Users\ELCOT\Documents\Chandru\Git_Bash\Attendance_system_using_facial_recognition\CODE\db\test.db'
+	table_name = "register"
+	updating_register_table_with_face_image(db_loc, table_name, "RA1711004040036", r"C:\Users\ELCOT\Documents\Chandru\Git_Bash\Attendance_system_using_facial_recognition\CODE\Classroom test\After face detection and croping\Cls1.png")
+
